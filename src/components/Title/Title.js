17ef9f6eca7a46a2e204598bb71fb927ae43a2a4
Aparna Shah <72094753+aparnashah2408@users.noreply.github.com>
2023-03-27 13:59:16 +0530

    fixes

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,9 +1,11 @@
 import React from "react";
 // import PropTypes from "prop-types";
 // import circle from "../../images/circle.png"
-import ceramics from "../../images/ceramics.jpg"
+import ceramics from "../../images/ceramic2.jpg"
 import "./Title.css";
-import { motion} from 'framer-motion/dist/framer-motion'
+
+
+// import { motion} from 'framer-motion/dist/framer-motion'
 
 
 
@@ -16,30 +18,36 @@ function Title(props) {
       
       
       <div className="right-side">
+      <div className="circle" ></div>
         <div className="text">
-            <p id="wordident">Tiles  Sanitaryware Doors</p>
-            <h1 >Ceramics Name</h1>
+            <div id="wordident"><div>Floor Tiles  </div> <div>  Wall Tiles  </div> <div>  Sanitaryware  </div> </div>
+            <h1 >Shri Akshara Tile World</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <motion.div 
+            {/* <motion.div 
             animate={{ x: move ? 200 : -200 }}
-            // transition = {{ delay: 2 }}
+            transition = {{ delay: 2 }}
             onClick = {() => {
               setMove(!move);
             }}
-            className="morph">
+            className="morph"> */}
               {/* <div className="glass"> */}
 
-              {/* <h4 className="text-center text-dark name" > */}
-              {/* Hi I am, <span className="text-warning font1">Aparna Shah.</span>  */}
-              {/* <br/> */}
-              See our <span className="text-warning font1">Products</span>
-              {/* </h4> */}
+              <h4 className="text-center text-dark name" >
+              See our <span id="red">Products</span> &rarr;
+              </h4>
+              
+              
+
+              
               {/* </div> */}
-            </motion.div>
+            {/* </motion.div> */}
         </div>
       </div>
+      <div className="circle2" ></div>
       <div className="ceramicsimg">
-      <img src={ceramics} ></img>
+        
+        <img src={ceramics} ></img>
+        
       </div>
 
     </div>
